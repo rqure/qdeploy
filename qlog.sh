cd68f863ffc717b3933d3c6b23c3d78cecbf0f89
rqure <64617806+rqure@users.noreply.github.com>
2024-01-25 09:57:39 -0700

    Update qlog.sh

--- qlog.sh
+++ qlog.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Check if exactly one argument is given
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <app name>"
+  exit 1
+fi
+
+IMAGE=rqure/logger:$IMAGE_VERSION
+
+APP_NAME=$1
+
+docker run --attach -e APP_NAME $IMAGE
