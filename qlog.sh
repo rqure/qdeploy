9b06a3265271fa198bb9955dfdb251ad0961faa1
rqure <64617806+rqure@users.noreply.github.com>
2024-01-25 14:21:23 -0700

    Update qlog.sh

--- qlog.sh
+++ qlog.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# Function to handle the SIGINT signal (Ctrl+C)
+cleanup() {
+    echo "Caught SIGINT signal. Stopping the Docker container..."
+    docker stop "$CONTAINER_ID"
+    exit 0
+}
+
 # Check if exactly one argument is given
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <app name>"
   exit 1
 fi
 
-IMAGE=rqure/logger:v1.0.1
-
+NETWORK=qservice_default
+IMAGE=rqure/logger:v1.0.2
 APP_NAME=$1
 
-docker run --network qservice_default -e APP_NAME=$APP_NAME -it $IMAGE
+# Trap the SIGINT signal
+trap cleanup SIGINT
+
+# Run docker and get the container ID
+CONTAINER_ID=$(docker run --detach --network $NETWORK --attach STDOUT --attach STDERR -e APP_NAME=$APP_NAME $IMAGE)
+
+# Wait for the container to stop
+docker wait "$CONTAINER_ID"
