e2f8a9c5756ca6eddf6f5c28389c09a841d35ebe
rqure <64617806+rqure@users.noreply.github.com>
2024-04-21 19:02:01 +0000

    openvpn

--- qdeploy.sh
+++ qdeploy.sh
@@ -1,12 +1,13 @@
 # Set environment
 export HOST_ADDR=$(curl -s https://api.ipify.org)
+export VPN_ANONYMOUS_CLIENT_NAME="qanonymous"
 
 # Create volumes for config and data
 mkdir -p volumes/openvpn/
 if [ ! "$(ls -A volumes/openvpn/)" ]; then
     docker run -v ./volumes/openvpn:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://vpn.qmq
     docker run -v ./volumes/openvpn:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
-    docker run -v ./volumes/openvpn:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full qanonymous nopass
+    docker run -v ./volumes/openvpn:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $VPN_ANONYMOUS_CLIENT_NAME nopass
 fi
 
 mkdir -p volumes/qexchange/
@@ -17,6 +18,11 @@ fi
 mkdir -p volumes/qzigbee2mqtt/
 if [ ! -f volumes/qzigbee2mqtt/configuration.yaml ]; then
     curl https://raw.githubusercontent.com/rqure/qzigbee2mqtt/main/configuration.yaml -o volumes/qzigbee2mqtt/configuration.yaml
+fi
+
+mkdir -p volumes/certs/
+if [ ! "$(ls -A volumes/certs/)" ]; then
+    docker run -v ./volumes/openvpn:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $VPN_ANONYMOUS_CLIENT_NAME > $VPN_ANONYMOUS_CLIENT_NAME.ovpn
 fi
 
 # Deploy the stack
