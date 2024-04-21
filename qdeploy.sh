6d00232ec7c19faa76159d1a8e6e6ab9f40c5c95
rqure <64617806+rqure@users.noreply.github.com>
2024-04-21 16:43:10 +0000

    some progress

--- qdeploy.sh
+++ qdeploy.sh
@@ -1,1 +1,18 @@
-docker stack deploy --compose-file=docker-compose.yml qservice
+# Create .env file
+export HOST_ADDR=$(curl -s https://api.ipify.org)
+
+# Create volumes for config and data
+mkdir -p volumes/openvpn/
+
+mkdir -p volumes/qexchange/
+if [ ! -f volumes/qexchange/exchanges.json ]; then
+    curl https://raw.githubusercontent.com/rqure/qexchange/main/exchanges.json -o volumes/qexchange/exchanges.json
+fi
+
+mkdir -p volumes/qzigbee2mqtt/
+if [ ! -f volumes/qzigbee2mqtt/configuration.yaml ]; then
+    curl https://raw.githubusercontent.com/rqure/qzigbee2mqtt/main/configuration.yaml -o volumes/qzigbee2mqtt/configuration.yaml
+fi
+
+# Deploy the stack
+# docker stack deploy --compose-file=docker-compose.yml qservice
