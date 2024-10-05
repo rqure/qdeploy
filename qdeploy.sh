838c86658c91db1abf5d63ccce36393c6e118715
rqure <64617806+rqure@users.noreply.github.com>
2024-10-05 17:26:33 -0600

    Update qdeploy.sh

--- qdeploy.sh
+++ qdeploy.sh
@@ -162,6 +162,48 @@ server {
 
 server {
     listen 80;
+
+    location /garage {
+        proxy_pass http://garage:20001;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+}
+
+server {
+    listen 80;
+
+    location /logs {
+        proxy_pass http://dozzle:8080;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+}
+
+server {
+    listen 80;
+
+    location /z2m {
+        proxy_pass http://zigbee2mqtt:8080;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+}
+
+server {
+    listen 80;
     server_name logs.local;
 
     location / {
