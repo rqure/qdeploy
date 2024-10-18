0c2d86f2fd1aebce5e0d692c44d3c7727c0ad3fa
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-10-18 18:27:40 +0000

    Add longer timeout for restore

--- restore_snapshot.sh
+++ restore_snapshot.sh
@@ -7,10 +7,10 @@ wget -O snapshot.tar.gz https://github.com/rqure/qsnapshot/releases/download/$VE
 tar -xzvf snapshot.tar.gz
 
 # Restore snapshot
-ID=$(curl -s localhost/make-client-id | jq -r '.header.id')
+ID=$(curl -s "localhost/make-client-id?clientTimeout=60" | jq -r '.header.id')
 SNAPSHOT=$(cat tmp/snapshot.json | jq ".header.id = \"$ID\"")
 echo $SNAPSHOT > tmp/snapshot.json
-curl http://localhost/api -d @./tmp/snapshot.json
+curl "http://localhost/api?requestTimeout=60" -d @./tmp/snapshot.json
 
 # Cleanup
 rm -rf tmp snapshot.tar.gz
