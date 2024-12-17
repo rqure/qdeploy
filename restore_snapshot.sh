a374894bf34f4f779ac7bbeb1fd4eac9c12d6b04
rqure <64617806+rqure@users.noreply.github.com>
2024-12-17 14:04:28 -0700

    Update restore_snapshot.sh

--- restore_snapshot.sh
+++ restore_snapshot.sh
@@ -1,3 +1,18 @@
-wget -O /tmp/snapshot.json https://github.com/rqure/qsnapshot/releases/download/v0.0.1/snapshot.json
-curl http://localhost/api -d @/tmp/snapshot.json
-rm -f /tmp/snapshot.json
+VERSION=v0.0.6
+
+# # Download from release page
+wget -O snapshot.tar.gz https://github.com/rqure/qsnapshot/releases/download/$VERSION/snapshot.tar.gz
+
+# # Extract to qtmp/snapshot.json
+tar -xzvf snapshot.tar.gz
+
+# Restore snapshot
+ID=$(curl -s "localhost/make-client-id?clientTimeout=60" | jq -r '.header.id')
+SNAPSHOT=$(cat tmp/snapshot.json | jq ".header.id = \"$ID\"")
+echo $SNAPSHOT > tmp/snapshot.json
+curl "http://localhost/api?requestTimeout=60" -d @./tmp/snapshot.json
+
+# Cleanup
+rm -rf tmp snapshot.tar.gz
+
+echo
