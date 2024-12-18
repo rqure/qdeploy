9e765e2cd7e385850869c5e18ddbf3a0124d5db1
rqure <64617806+rqure@users.noreply.github.com>
2024-12-18 15:05:01 -0700

    Update qdeploy.sh

--- qdeploy.sh
+++ qdeploy.sh
@@ -148,21 +148,6 @@ server {
 
 server {
     listen 80;
-    server_name pihole.local;
-
-    location / {
-        proxy_pass http://pihole:80;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-    }
-}
-
-server {
-    listen 80;
     server_name database.local;
 
     location / {
