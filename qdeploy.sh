e6018750a80e3e9b891b6b356dc3f788fd8dfb76
rqure <64617806+rqure@users.noreply.github.com>
2024-12-19 21:26:24 -0700

    Update qdeploy.sh

--- qdeploy.sh
+++ qdeploy.sh
@@ -192,7 +192,7 @@ services:
     environment:
       - Q_IN_DOCKER=true
   qdp:
-    image: rqure/qdp:v0.0.5
+    image: rqure/qdp:v0.0.6
     restart: always
     environment:
       - Q_IN_DOCKER=true
