5ee1273bb61d28246b0d911409520fddce1987c3
rqure <64617806+rqure@users.noreply.github.com>
2024-12-20 19:55:30 -0700

    Update qdeploy.sh

--- qdeploy.sh
+++ qdeploy.sh
@@ -175,24 +175,24 @@ services:
     ports:
       - "6379:6379"
   clock:
-    image: rqure/clock:v2.3.1
+    image: rqure/clock:v2.3.2
     restart: always
     environment:
       - Q_IN_DOCKER=true
   qsm:
-    image: rqure/qsm:v0.1.1
+    image: rqure/qsm:v0.1.3
     restart: always
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
     environment:
       - Q_IN_DOCKER=true
   webgateway:
-    image: rqure/webgateway:v0.1.2
+    image: rqure/webgateway:v0.1.3
     restart: always
     environment:
       - Q_IN_DOCKER=true
   qdp:
-    image: rqure/qdp:v0.0.8
+    image: rqure/qdp:v0.0.9
     restart: always
     environment:
       - Q_IN_DOCKER=true
